c109711ce4ec9eb5f056dec5278f2a79553c9d87
machinegunbryce <brchan1@s.sfusd.edu>
2025-02-03 22:04:50 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -37,13 +37,17 @@ public void tester()
 }
 public boolean palindrome(String word)
 {
-  //your code here
+  if(word.equals(reverse(word))){
+    return true;
+  }
   return false;
 }
 public String reverse(String str)
 {
     String sNew = new String();
-    //your code here
+      for( int i = str.length()-1; i >= 0; i--){
+        sNew += str.substring(i , i +1);
+      }
     return sNew;
 }
 }
